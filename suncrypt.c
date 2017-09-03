191b01505ecd82ab7bb491c159dc71434b41bad0
chandyt <timmychandy@hotmail.com>
2017-09-02 22:13:58 -0500

    generate key working

--- suncrypt.c
+++ suncrypt.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 #include<gcrypt.h>
 
-void main()
+void main(int argc, char *argv[])
 {
-    printf("Hello World");
+	char strPassword[50];	// TODO: Change to char *
+	char *fileName;
+	char *strSalt = "NaCl";
+	char *strKey= (char *) malloc(32);
+	printf("Enter Password: ");
+	scanf("%s", strPassword);
+
+	fileName = argv[1];
+	printf(fileName); //TODO: Remove me
+
+	int errHandler= gcry_kdf_derive(strPassword, strlen(strPassword), GCRY_KDF_PBKDF2, GCRY_MD_SHA512, strSalt, strlen(strSalt), 4096,32, strKey);
 
 
+	int i=0;
+	for(i=0; i<32; i++){
+		printf("%02X " ,strKey[i]);
+	}
+
+	
 }
 
 
